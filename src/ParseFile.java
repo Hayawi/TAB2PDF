4f42e436c63759dc80a4afec1659b76fe70551fe
Yahya <yahya.ismail@hotmail.ca>
2015-03-25 12:40:34 -0400

    Don't change this or I'll end you

--- src/ParseFile.java
+++ src/ParseFile.java
@@ -1,8 +1,7 @@
-
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.regex.Matcher;
@@ -25,36 +24,17 @@ public class ParseFile {
 		    return file;
 	}
 	
-	public static ArrayList staffSort(String body) {
-		// Queue<String> stringQueue = new Queue<String>();
-		String bar = "";
+	public static ArrayList<Measure> sortMeasure(String body) {
 		String line = "";
-		
-		//a counter to keep track of how many bars we have, and what bar we're on
-		int numberOfStaves = 0;
-		int indexOfStaff = 0;
-		
-		// we store the index of the newline character and the index of the vertical line for easy use
 		int indexOfNewLine = 0;
 		int indexOfVerticalLine = 0;
-		int indexOfLastVerticalLine = 0;
+		int lineLength = 0;
 		Pattern pattern = Pattern.compile("\\|?(\\||(\n[0-9]))(-|\\*)");
-
-		// we need to keep track of what number of the 6 lines per bar we're on
-		int lineIndex = 0;
-		
-		// we need to keep track if we're on the first line or not (we set this value to true when we finish with a set of bars
-		boolean firstLine = true;
-		
-		// to keep track of the queues with an index
-		ArrayList<Queue<ArrayList<String>>> bars = new ArrayList<Queue<ArrayList<String>>>();
 		ArrayList<String> blockOfMeasures = new ArrayList<String>();
 		ArrayList<Measure> measures = new ArrayList<Measure>();
 		
-		//until we're done with the entire string (while the index of the new line character is 0 or more, or the length of the string is greater than 0
+		// forgot why body length > 0 is used, check again later
 		while (indexOfNewLine != -1 && body.length() > 0) {
-
-
 			for (int i = 0; i < 6; i ++) { 
 				Matcher matcher = pattern.matcher(body);
 				if (matcher.find()) {
@@ -62,44 +42,18 @@ public class ParseFile {
 				}
 				indexOfNewLine = body.indexOf('\n', indexOfVerticalLine + 1);
 				line = body.substring(indexOfVerticalLine, indexOfNewLine).trim(); // this takes the entire line
-				
-				
 				if (line.lastIndexOf("| ") > 0) 
 					line = line.substring(0, line.lastIndexOf("| ") + 1).trim(); // quick and dirty fix
-
 				body = body.substring(indexOfNewLine + 1); 
-				
-				
-	/*			int index = 0;
-				matchSeparators.find();
-				while (matchSeparators.find()) {
-					bar = line.substring(index, matchSeparators.end());
-					index = matchSeparators.start();
-					System.out.println(bar);
-
-					ArrayList<String> parsedBar = parse(bar);
-					System.out.println(firstLine);
-					if (firstLine) {
-						bars.add(new LinkedList<ArrayList<String>>());
-						numberOfStaves++;
-						bars.get(indexOfStaff).add(parsedBar);
-						indexOfStaff++;
-					}
-					else {
-						bars.get(indexOfStaff).add(parsedBar);
-						indexOfStaff++;
-					} 
-				} */
-				
 				blockOfMeasures.add(line);
-				indexOfStaff = indexOfStaff - numberOfStaves; 				
-
 			}
-			measures.addAll(something(blockOfMeasures));
+//			if (!unevenBlockLengthCheck(blockOfMeasures)) {
+	//			blockOfMeasures = reconstruct(blockOfMeasures);
+		//	}
+			measures.addAll(convertToMeasures(blockOfMeasures));
 			blockOfMeasures.clear();
-
-			
 			body = body.substring(body.indexOf('\n') + 1);
+			
 			if (body.indexOf('\n') <= 1) { // something idk check again later
 				while (body.indexOf('\n') >= 0 && body.indexOf('\n') <= 1)
 				body = body.substring(body.indexOf('\n') + 1);
@@ -184,28 +138,47 @@ public class ParseFile {
 		}
 		return token;
 	}
-	public static ArrayList<Measure> something(ArrayList<String> block) {
+	
+	public static ArrayList<Measure> convertToMeasures(ArrayList<String> block) {
 		Pattern separator = Pattern.compile("(\\||^[0-9])([0-9]|\\|)?");
 		Matcher matcher = separator.matcher(block.get(2));
-		ArrayList<String> measures = new ArrayList<String>();
+		ArrayList<String> measure = new ArrayList<String>();
 		ArrayList<Measure> newMeasures = new ArrayList<Measure>();
-		int index = 0;
-		
+		int indexOfBeginningStaff = 0;
 		matcher.find();
 		while (matcher.find()) {
 			for (String s : block) {
-				measures.add(s.substring(index, matcher.end()));
+				measure.add(s.substring(indexOfBeginningStaff, matcher.end()));
 			}
-			newMeasures.add(new Measure(measures));
-			measures.clear();
-			index = matcher.start();
+			newMeasures.add(new Measure(measure));
+			measure.clear();
+			indexOfBeginningStaff = matcher.start();
 		}
-		
 		return newMeasures;
 	}
 	
-	public static ArrayList<String> reconstruct(ArrayList<String> tokens) {
-		return null; // parse, reconstruct. then pass to something() if we have two tokens that are adjacent numbers, insert a space. blah blah blah do shit, only call this if
-					 // the lengths of all 6 lines of the measure are not the same.
+	public static boolean unevenBlockLengthCheck(ArrayList<String> blockOfMeasures) {
+		HashSet<Integer> lengths = new HashSet<Integer>();
+		for (String s : blockOfMeasures) {
+			lengths.add(s.length());
+		}
+		if (lengths.size() > 1)
+			return true;
+		return false;
+	}
+	public static ArrayList<String> reconstruct(ArrayList<String> block) {
+		StringBuilder string = new StringBuilder();
+		ArrayList<ArrayList<String>> tokens = new ArrayList<ArrayList<String>>();
+		ArrayList<String> processedBlock = new ArrayList<String>();
+		for (String line : block) {
+			tokens.add(parse(line));
+		}
+		for (ArrayList<String> line : tokens) {
+			for (String token : line) {
+				string.append(token);
+			}
+			processedBlock.add(string.toString());
+		}
+		return null; 
 	}
 }
